0fc9cc7a45267dea0c58b104b9f973999d31b037
Josip Igrec <me@allypost.net>
2021-11-06 19:47:00 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,33 @@
+FROM ubuntu:20.04
+
+RUN mkdir -p /app
+
+WORKDIR /app
+
+RUN apt-get update && apt-get install -y \
+  bash \
+  gcc \
+  g++ \
+  make \
+  curl
+
+# Install NodeJS
+RUN curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
+RUN apt-get install -y nodejs
+
+# Install yarn
+RUN curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | tee /usr/share/keyrings/yarnkey.gpg >/dev/null && echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | tee /etc/apt/sources.list.d/yarn.list && apt-get update && apt-get install yarn
+
+# Install dependencies for playwright
+RUN npx playwright install-deps
+
+# Install PM2 (process manager)
+RUN npm i -g pm2
+
+COPY yarn.lock package.json ./
+
+RUN yarn install --frozen-lockfile
+
+COPY . .
+
+CMD ["pm2", "start", "--name", "Twitter Screenshotter", "--no-daemon", "-i", "max", "index.js"]
---
+++ index.js
@@ -0,0 +1,137 @@
+const { chromium, Browser } = require('playwright');
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { StatusCodes } = require('http-status-codes');
+const morgan = require('morgan');
+const slowDown = require("express-slow-down");
+const RedisStore = require("rate-limit-redis");
+
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 80;
+
+const REQUESTS_PER_SECOND = 1;
+const REQUESTS_MEASURE_WINDOW_SECONDS = 1 * 60; // 1 minute
+
+const EMBED_HTML = fs.readFileSync('./embed.html', 'utf-8');
+
+/**
+ * @type {Browser}
+ */
+let BROWSER;
+
+const asyncReq =
+  (handler) => async (req, res) => {
+    try {
+      return await handler(req, res);
+    } catch {
+      return res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+    } finally {
+      try {
+        await req.$page.page.close();
+      } catch { }
+
+      try {
+        await req.$page.context.close();
+      } catch { }
+    }
+  }
+  ;
+
+const app = express();
+
+app.disable('x-powered-by');
+app.enable("trust proxy");
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+
+const speedLimiter = slowDown({
+  windowMs: REQUESTS_MEASURE_WINDOW_SECONDS * 1000,
+  delayAfter: REQUESTS_PER_SECOND * REQUESTS_MEASURE_WINDOW_SECONDS,
+  delayMs: 734,
+  headers: true,
+  store: new RedisStore({
+    redisURL: process.env.REDIS_URL,
+  }),
+});
+
+app.post("/", speedLimiter, asyncReq(async (req, res) => {
+  const { url: twitterUrl } = req.body;
+
+  if (!twitterUrl) {
+    return res.sendStatus(StatusCodes.NOT_ACCEPTABLE);
+  }
+
+  const parsedUrl = new URL(twitterUrl);
+
+  if (parsedUrl.hostname !== 'twitter.com') {
+    return res.sendStatus(StatusCodes.NOT_ACCEPTABLE);
+  }
+
+  req.$page = {};
+
+  req.$page.context = await BROWSER.newContext({
+    acceptDownloads: false,
+    screen: {
+      width: 1920,
+      height: 1080,
+    },
+  });
+  req.$page.page = await req.$page.context.newPage();
+  await req.$page.page.setContent(EMBED_HTML.replace(
+    '{{URL_FOR_TWITTER}}',
+    twitterUrl,
+  ));
+
+  const tweetIframe = await req.$page.page.waitForSelector('.twitter-tweet-rendered iframe');
+  const frame = await tweetIframe.contentFrame();
+
+  {
+    const retweetLink = await frame.$$('a[role="link"]').then((links) => links.pop());
+
+    retweetLink.evaluate((el) => {
+      const $retweetDiv = el.parentNode;
+      $retweetDiv.parentNode.removeChild($retweetDiv);
+    });
+  }
+
+  {
+    const copyLinkToTweetLink = await frame.$('a[role="link"][aria-label^="Like."]');
+
+    copyLinkToTweetLink.evaluate((el) => {
+      const $actions = el.parentNode;
+      const $copyLinkToTweet = $actions.querySelector('div[role="button"]');
+
+      $copyLinkToTweet.parentNode.removeChild($copyLinkToTweet);
+    });
+  }
+
+  const tweet = await frame.$('#app');
+
+  const buffer = await tweet.screenshot({
+    omitBackground: true,
+    type: "png",
+  });
+
+  res.setHeader('Content-Type', 'image/png');
+  res.setHeader('Content-Length', buffer.length);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  return res.end(buffer);
+}));
+
+Promise.resolve()
+  .then(async () => {
+    BROWSER = await chromium.launch();
+  })
+  .then(() => {
+    app.listen(
+      PORT,
+      HOST,
+      () => {
+        console.error(`|> Listening on http://${HOST}:${PORT}`);
+      },
+    );
+  })
+  ;
