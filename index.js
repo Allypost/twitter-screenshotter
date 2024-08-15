f32fe10b16a01fda4a93d9b3a07af0f2b2f95ba3
Josip Igrec <josip@igr.ec>
2024-08-15 16:41:41 +0200

    Add misskey/sharkey support

--- index.js
+++ index.js
@@ -898,6 +898,135 @@ const handleTumblrPost = async (req, res, url) => {
  * @param {AppResponse} res
  * @param {URL} url
  */
+const handleMisskeyPost = async (req, res, url) => {
+  logger.debug("Misskey post", url.toString());
+
+  const urlPath = url.pathname.replace(/\/$/, "");
+  const postId = urlPath.split("/").pop() ?? "";
+
+  const context = await newBrowserContext();
+  req.$browserContext = context;
+
+  const buffer = await (async (context, url) => {
+    logger.debug("Start rendering Misskey page", url.toString());
+    const page = await context.newPage();
+
+    await page.goto(url.toString());
+
+    await page.waitForLoadState("networkidle");
+
+    const post$ = await page.$("main article").catch(() => null);
+
+    if (!post$) {
+      logger.debug("Post not available");
+      return null;
+    }
+
+    const container$ = await post$.evaluateHandle(($post) => {
+      /**
+       * @type {HTMLElement}
+       */
+      const $parent = $post.parentElement;
+      return $parent;
+    });
+
+    if (!(await container$.evaluate((x) => x))) {
+      logger.debug("Container not found");
+      return null;
+    }
+
+    // Remove sticky header
+    {
+      await page.evaluate(() => {
+        document.querySelector("main > div > div")?.remove();
+      });
+    }
+
+    // Remove everything except the post from the container
+    {
+      await container$.evaluate(($container) => {
+        let nextElement = $container?.firstChild;
+        while (nextElement) {
+          let elementToRemove = nextElement;
+          nextElement = nextElement.nextSibling; // Move to the next sibling
+          if (elementToRemove.name) {
+            elementToRemove.parentNode.removeChild(elementToRemove); // Remove the current sibling
+          }
+        }
+      });
+    }
+
+    // Remove replies to post
+    {
+      await post$.evaluate(($post) => {
+        let nextElement = $post?.nextSibling;
+        while (nextElement) {
+          let elementToRemove = nextElement;
+          nextElement = nextElement.nextSibling; // Move to the next sibling
+          elementToRemove.parentNode.removeChild(elementToRemove); // Remove the current sibling
+        }
+      });
+    }
+
+    // Remove pure post actions and add style to footer
+    {
+      await container$.evaluate(($container) => {
+        const $footers = $container.querySelectorAll("footer");
+        for (const $footer of $footers) {
+          let nextElement =
+            $footer.parentElement.querySelector("footer > button")?.nextSibling
+              ?.nextSibling;
+          while (nextElement) {
+            let elementToRemove = nextElement;
+            nextElement = nextElement.nextSibling; // Move to the next sibling
+            elementToRemove.parentNode.removeChild(elementToRemove); // Remove the current sibling
+          }
+
+          $footer.style.justifyContent = "flex-start";
+        }
+      });
+    }
+
+    // Open all summaries
+    {
+      await container$.evaluate(($container) => {
+        $container.querySelectorAll("details > summary").forEach(($summary) => {
+          $summary.click();
+          $summary.style.display = "none";
+        });
+      });
+
+      await page.waitForLoadState("networkidle");
+    }
+
+    return container$.screenshot(SCREENSHOT_CONFIG);
+  })(context, url).catch(() => null);
+
+  if (!buffer) {
+    return res.sendStatus(StatusCodes.NOT_FOUND);
+  }
+
+  res.setHeader("Content-Type", `image/${SCREENSHOT_CONFIG.type}`);
+  res.setHeader("Content-Length", buffer.length);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Cache-Control",
+    `public, max-age=${SEND_CACHE_HEADER_FOR_SECONDS}, s-max-age=${SEND_CACHE_HEADER_FOR_SECONDS}`,
+  );
+  res.setHeader(
+    "Content-Disposition",
+    `inline; filename="misskey-post.${url.hostname}.${postId}.${SCREENSHOT_CONFIG.type}"`,
+  );
+
+  return res.end(buffer);
+};
+
+/**
+ *
+ * @param {AppRequest} req
+ * @param {AppResponse} res
+ * @param {URL} url
+ */
 const handleActivityPub = async (req, res, url) => {
   const getSofwareName = async () => {
     const nodeInfoListValidator = z.object({
@@ -966,6 +1095,8 @@ const handleActivityPub = async (req, res, url) => {
 
   const instanceHandlers = {
     mastodon: () => handleMastodonToot(req, res, url),
+    misskey: () => handleMisskeyPost(req, res, url),
+    sharkey: () => handleMisskeyPost(req, res, url),
   };
 
   if (!req.$seenUrls) {
